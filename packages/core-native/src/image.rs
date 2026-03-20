9a94f141119edd5084b605afccbb0d38b7c70180
João Ferro <joaoferro.business@gmail.com>
2026-03-20 11:49:04 +0000

    feat(core): add ico and svg output support across native, wasm, and desktop

--- packages/core-native/src/image.rs
+++ packages/core-native/src/image.rs
@@ -27,6 +27,23 @@ pub fn convert(input: &Path, output: &Path, quality: u8) -> Result<(), CoreError
         "avif" => {
             img.save_with_format(output, image::ImageFormat::Avif)?;
         }
+        "ico" => {
+            img.save_with_format(output, image::ImageFormat::Ico)?;
+        }
+        "svg" => {
+            use base64::Engine as _;
+            use std::io::Write;
+            let mut png_buf = Vec::new();
+            img.write_to(&mut std::io::Cursor::new(&mut png_buf), image::ImageFormat::Png)?;
+            let b64 = base64::engine::general_purpose::STANDARD.encode(&png_buf);
+            let w = img.width();
+            let h = img.height();
+            let svg = format!(
+                r#"<svg xmlns="http://www.w3.org/2000/svg" width="{w}" height="{h}" viewBox="0 0 {w} {h}"><image href="data:image/png;base64,{b64}" width="{w}" height="{h}"/></svg>"#
+            );
+            let mut out_file = std::fs::File::create(output)?;
+            out_file.write_all(svg.as_bytes())?;
+        }
         fmt => {
             return Err(CoreError::UnsupportedFormat(fmt.to_string()));
         }
